7413556076db7bc83ce15510a9d6a1690a981bdf
Jake <git@ja-ke.tech>
2020-02-09 18:00:38 +0100

    Add man colors

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -2,6 +2,14 @@
 set -x VISUAL nvim
 set -x EDITOR nvim
 
+# man/less
+set -x LESS_TERMCAP_md \e\[01\;31m
+set -x LESS_TERMCAP_me \e\[0m
+set -x LESS_TERMCAP_se \e\[0m
+set -x LESS_TERMCAP_so \e\[01\;44\;33m
+set -x LESS_TERMCAP_ue \e\[0m
+set -x LESS_TERMCAP_us \e\[01\;32m
+
 # tmpdir
 if not set -q TMPDIR
   set -x TMPDIR /tmp
