06fdf8a53d802492d151c89a7bb549469b69e6c0
Jake <git@ja-ke.tech>
2020-04-11 00:55:56 +0200

    Add aur clone function

---
+++ .config/fish/functions/clone-aur.fish
@@ -0,0 +1,6 @@
+function clone-aur --description 'Clone aur repo by name'
+	set repo $argv[1];
+	
+	git clone "ssh://aur@aur.archlinux.org/$repo.git"
+	or git clone "https://aur.archlinux.org/$repo.git"
+end
