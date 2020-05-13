95e659e08fff210d755052a75819ffb02ca8e01a
Jake <git@ja-ke.tech>
2020-05-13 13:27:58 +0200

    Improve aur scripts

--- .config/fish/functions/clone-aur.fish
+++ .config/fish/functions/clone-aur.fish
@@ -3,4 +3,6 @@ function clone-aur --description 'Clone aur repo by name'
 	
 	git clone "ssh://aur@aur.archlinux.org/$repo.git"
 	or git clone "https://aur.archlinux.org/$repo.git"
+
+	cd $repo
 end
--- .config/fish/functions/upd-aur.fish
+++ .config/fish/functions/upd-aur.fish
@@ -1,6 +1,8 @@
 function upd-aur --description 'Update pkgbuild to new version'
 	set new_ver $argv[1];
 	sed -E "s#(pkgver=).*#\1$new_ver#" -i PKGBUILD
+	set new_rel 1;
+	sed -E "s#(pkgrel=).*#\1$new_rel#" -i PKGBUILD
 	
 	updpkgsums
 	makepkg --printsrcinfo > .SRCINFO
