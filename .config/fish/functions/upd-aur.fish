1d2873dd02a587b77b3aa0cc8b8b6012246df0cf
Jake <git@ja-ke.tech>
2020-07-31 18:58:36 +0200

    Fix update aur script for custom vars

--- .config/fish/functions/upd-aur.fish
+++ .config/fish/functions/upd-aur.fish
@@ -1,8 +1,8 @@
 function upd-aur --description 'Update pkgbuild to new version'
 	set new_ver $argv[1];
-	sed -E "s#(pkgver=).*#\1$new_ver#" -i PKGBUILD
+	sed -E "s#(^pkgver=).*#\1$new_ver#" -i PKGBUILD
 	set new_rel 1;
-	sed -E "s#(pkgrel=).*#\1$new_rel#" -i PKGBUILD
+	sed -E "s#(^pkgrel=).*#\1$new_rel#" -i PKGBUILD
 	
 	updpkgsums; or return 1
 	makepkg --printsrcinfo > .SRCINFO
