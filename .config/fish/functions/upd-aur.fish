92be3d9bda987c8283741847a6b6d12915cc05c0
Jake <git@ja-ke.tech>
2021-07-21 00:26:17 +0200

    Add pkgrel to AUR update function

--- .config/fish/functions/upd-aur.fish
+++ .config/fish/functions/upd-aur.fish
@@ -1,11 +1,11 @@
 function upd-aur --description 'Update pkgbuild to new version'
 	set new_ver $argv[1];
 	sed -E "s#(^pkgver=).*#\1$new_ver#" -i PKGBUILD
-	set new_rel 1;
+	set new_rel 1; set -q $argv[2] || set new_rel $argv[2];
 	sed -E "s#(^pkgrel=).*#\1$new_rel#" -i PKGBUILD
 	
 	updpkgsums; or return 1
 	makepkg --printsrcinfo > .SRCINFO
 	
-	git commit -v -a -m "Update to $new_ver"
+	git commit -v -a -m "Update to $new_ver ($new_rel)"
 end
